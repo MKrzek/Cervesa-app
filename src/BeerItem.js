8326ea6f00dad54facc4626e884813d5a934cf76
Malgorzata <mkrzek@googlemail.com>
2017-12-14 15:38:39 +0000

    added new component to display individual item

--- src/BeerItem.js
+++ src/BeerItem.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import _ from 'lodash';
+
 export default class BeerItem extends React.Component{
     
     ShowItem=()=>{
-        console.log ( 'one beer', this.props.beer)
-        return _.map(this.props.beer, item=>{
+        console.log ('one beer', this.props.beer)
             return <div>
-                     {item.name}
-                     {item.tagline}
-                    <img src={item.img} alt='beer'/>
+                     {this.props.beer.name}
+                     {this.props.beer.tagline}
+                    <img src={this.props.beer.image_url} alt='beer'/>
                     </div>
-        })
+       
     }
 
     render(){
-        
         return <div>
+                  {this.ShowItem()}
                </div>
     }
 }
