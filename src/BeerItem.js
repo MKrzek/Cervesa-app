4e9394d05bfdd8f343bb7c688a1116237ed809fd
Malgorzata <mkrzek@googlemail.com>
2017-12-27 13:21:49 +0000

    styled the title page

--- src/BeerItem.js
+++ src/BeerItem.js
@@ -7,10 +7,12 @@ export default class BeerItem extends React.Component{
         const{name, tagline, image_url, id}=this.props.beer;
             
             return   <div onClick={this.showDetail} className='col card beerItem'>
-                            <div className = 'text-center'> <img src={image_url} alt='beer'/> </div>
-                             <div className = 'text-center'> 
+                            <div className = 'text-center'> 
+                                    <img src={image_url} alt='beer'/> 
+                            </div>
+                            <div className = 'text-center'> 
                                  <Link className= 'btn btn-info btn-wrap-text' to={`/detail/${id}`}>{name}</Link> 
-                             </div>
+                            </div>
                             <p className='text-center tagLine'>{tagline}</p>
                      </div>
                   
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Navigation from './Navigation.js';
+
+
 export default class Main extends React.Component{
     render(){
         return(<div>
                   <Navigation />
-                    <h1>
-                        Your app beer
-                    </h1>
-                </div>
+                  <div className='container'>
+                     <div className='row jutify-content-center'>
+                        <div className='col text-center'>
+                          <div id = "bubbles"> 
+                                 <Link className='title' to='/detail'>Cervesas</Link>
+                                <p className='text-muted'>Explore the World of Beer</p>
+                            <div className="bubble bubble1"></div>
+                            <div className = "bubble bubble2"></div> 
+                            <div className = "bubble bubble3"></div> 
+                            <div className = "bubble bubble4"></div>
+                            <div className = "bubble bubble5"></div>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+               </div>
         )
     }
 }
